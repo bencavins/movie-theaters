f6876a5c1102dc4bd99e57c681bd0a5602be2fd1
Ben Cavins <ben.cavins@flatironschool.com>
2024-09-18 12:12:21 -0400

    add movie theaters

--- server/src/models.py
+++ server/src/models.py
@@ -1,8 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
+from sqlalchemy.ext.associationproxy import association_proxy
 
 
-db = SQLAlchemy()
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+
+db = SQLAlchemy(metadata=MetaData(naming_convention=convention))
 
 
 class Movie(db.Model, SerializerMixin):
@@ -10,7 +21,15 @@ class Movie(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-    director = db.Column(db.STring)
+    director = db.Column(db.String)
+
+    showtimes = db.relationship('ShowTime', back_populates='movie')
+    theaters = association_proxy('showtimes', 'theater')
+
+    serialize_rules = ['-showtimes.movie']
+
+    def __repr__(self) -> str:
+        return f'<Movie {self.title} {self.director}>'
 
 
 class ShowTime(db.Model, SerializerMixin):
@@ -18,11 +37,29 @@ class ShowTime(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
+    theater_id = db.Column(db.Integer, db.ForeignKey('theaters.id'))
+
+    movie = db.relationship('Movie', back_populates='showtimes')
+    theater = db.relationship('Theater', back_populates='showtimes')
+
+    serialize_rules = ['-movie.showtimes', '-theater.showtimes']
+
+    def __repr__(self) -> str:
+        return f'<ShowTime {self.time}>'
 
 
 class Theater(db.Model, SerializerMixin):
-    __tablename__ = 'theater'
+    __tablename__ = 'theaters'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     address = db.Column(db.String)
+
+    showtimes = db.relationship('ShowTime', back_populates='theater')
+    movies = association_proxy('showtimes', 'movie', creator=lambda movie_obj: ShowTime(movie=movie_obj))
+
+    serialize_rules = ['-showtimes.theater']
+
+    def __repr__(self) -> str:
+        return f'<Theater {self.name}>'
--- server/src/seed.py
+++ server/src/seed.py
@@ -1,10 +1,28 @@
 from app import app
 from models import db, Movie, ShowTime, Theater
+from datetime import datetime
 
 
 def run():
-    pass
+    shining = Movie(title='The Shining', director='Stanley Kubrick')
+    star_wars = Movie(title='Star Wars', director='George Lucas')
 
+    db.session.add_all([shining, star_wars])
+
+    amc = Theater(name='AMC')
+    alamo = Theater(name='Alamo Drafthouse')
+
+    db.session.add_all([amc, alamo])
+
+    times = [
+        ShowTime(movie=shining, theater=amc, time=datetime(year=2024, month=9, day=17, hour=12, minute=0, second=0)),
+        ShowTime(movie=star_wars, theater=amc, time=datetime(year=2024, month=9, day=17, hour=13, minute=0, second=0)),
+        ShowTime(movie=shining, theater=alamo, time=datetime(year=2024, month=9, day=17, hour=14, minute=0, second=0)),
+        ShowTime(movie=star_wars, theater=alamo, time=datetime(year=2024, month=9, day=17, hour=15, minute=0, second=0)),
+    ]
+
+    db.session.add_all(times)
+    db.session.commit()
 
 if __name__ == '__main__':
     with app.app_context():
